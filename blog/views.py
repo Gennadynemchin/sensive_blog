ff20fdca04155deaa8e71b37038b1cb02f12395b
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-04-11 13:08:14 +0300

    Show popular posts on main page;

--- blog/views.py
+++ blog/views.py
@@ -29,9 +29,7 @@ def serialize_tag(tag):
 
 
 def index(request):
-
     most_popular_posts = Post.objects.annotate(like_count=Count('likes')).order_by('-like_count')[0:5]
-
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
