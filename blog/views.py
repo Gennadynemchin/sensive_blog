a3db9e1034faef1ac498a580d7307994bd02de61
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-04-17 23:18:34 +0300

    Refactoring ORM queries;

--- blog/views.py
+++ blog/views.py
@@ -44,7 +44,7 @@ def serialize_tag(tag):
 
 
 def index(request):
-    most_popular_posts = Post.objects.annotate(like_count=Count('likes'), comments_count=Count('comments')).order_by('-like_count').prefetch_related('author')
+    most_popular_posts = Post.objects.annotate(like_count=Count('likes', distinct=True), comments_count=Count('comments', distinct=True)).order_by('-like_count').prefetch_related('author')[0:5]
 
     fresh_posts = Post.objects.annotate(comments_count=Count('comments')).order_by('published_at').prefetch_related('author')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -52,7 +52,7 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts[0:5]
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
         'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
