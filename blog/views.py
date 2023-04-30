c4cbfb9cc192a4c2285d2e61f440de2b5a44943a
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-04-30 21:37:56 +0300

    Reduce db queries number in tag_filter;

--- blog/views.py
+++ blog/views.py
@@ -7,20 +7,6 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
-'''def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }'''
-
-
 def serialize_post_optimized(post):
     return {
         'title': post.title,
@@ -53,7 +39,6 @@ def index(request):
         'popular_tags': most_popular_tags,
     }
     return render(request, 'index.html', context)
-
 
 
 def post_detail(request, slug):
@@ -92,20 +77,16 @@ def post_detail(request, slug):
 
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
-
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = Post.objects.popular().prefetch_related("author", "comments")[:5].fetch_with_comments_count()
-
-    related_posts = tag.posts.all().annotate(comments_count=Count('comments'))[:20]
+    most_popular_posts = Post.objects.popular().prefetch_related("author").fetch_with_comments_count()[:5]
+    related_posts = tag.posts.all().prefetch_related('tags', 'author').annotate(comments_count=Count('comments'))[:20]
 
     context = {
         'tag': tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
         'posts': [serialize_post_optimized(post) for post in related_posts],
-        'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts
-        ],
+        'most_popular_posts': most_popular_posts,
     }
     return render(request, 'posts-list.html', context)
 
