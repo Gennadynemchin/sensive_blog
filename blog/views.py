85ecd7a97ac9fdca8641fe12fd2b537d75426bc1
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-05-03 09:59:00 +0300

    Fixed comments in post_detail;

--- blog/views.py
+++ blog/views.py
@@ -51,7 +51,7 @@ def index(request):
 
 def post_detail(request, slug):
     post = Post.objects.annotate(likes_count=Count("likes")).get(slug=slug)
-    comments = Comment.objects.prefetch_related("author").filter(post=post)
+    comments = post.comments.all().prefetch_related("author")
     serialized_comments = []
     for comment in comments:
         serialized_comments.append(
